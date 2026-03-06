7ea101aece58a178447f905fd0a32defd43e2d8f
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2026-03-07 02:20:13 +0700

    fix: Add turbopack config to resolve build error

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  turbopack: {}, // Use Turbopack with default config
 };
 
 export default withPWA(nextConfig);
